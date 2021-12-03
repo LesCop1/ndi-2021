0eea01510be04d9eb2035e76e9471c5012634de0
Mathis Engels <mathisengels@yahoo.fr>
2021-12-03 07:11:03 +0100

    Prepared for pitch

---
+++ src/pages/Pitch.tsx
@@ -0,0 +1,14 @@
+import { Container, Typography } from "@mui/material";
+import MainLayout from "layouts/MainLayout";
+import React from "react";
+
+const Pitch: React.FC = () => (
+  <MainLayout>
+    <Container>
+      <Typography variant="h3">Pitch</Typography>
+      <Typography>LIEN YT</Typography>
+    </Container>
+  </MainLayout>
+);
+
+export default Pitch;

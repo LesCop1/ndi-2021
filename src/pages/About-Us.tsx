564bac149dcc9e5a9b9f568e001325055cc9d4a1
tzrph <TzRPheonix@gmail.com>
2021-12-03 07:04:13 +0100

    Translated for AboutUs

--- src/pages/About-Us.tsx
+++ src/pages/About-Us.tsx
@@ -12,30 +12,23 @@ const AboutUs: React.FC = () => {
       <div className="aboutUsPage">
         <Container>
           <Typography className="title" variant="h1">
-            {t("who-are-we")}
+            {t("AboutUs-1")}
           </Typography>
           <br />
           <Typography>
-            Ce site trouve ses origines dans les recherches généalogiques entreprises par Philippe Boutelier.
+            {t("AboutUs-2")}
           </Typography>
           <br />
           <Typography>
-            La généalogie n’est pas pour lui une course à l’arbre le plus gros , il préfère s’intéresser aux personnes
-            et à leur vie. C’est ainsi que, en 2012, il se penche sur l’histoire de Joseph Taylor, sauveteur du
-            Dunkerquois, grand-oncle préféré de son père. ll s’aperçoit alors, à travers la carrière de ce sauveteur que
-            l’histoire de ces personnes d’exception (de Bray-Dunes à Grand-Fort-Philippe) dort dans les archives de la
-            Bibliothèque Nationale de France.
+            {t("AboutUs-3")}
             <br />
-            Il décide de mettre en lumière le parcours de ces hommes guidés par l’abnégation, l’humanisme et le courage,
-            avec un mépris indéniable pour les risques encourus.
+            {t("AboutUs-4")}
           </Typography>
 
           <Typography>
-            « Les Corsaires Dunkerquois » : l’association philanthropique dunkerquoise remarque ce travail de mémoire et
-            passe une convention avec Philippe Boutelier de façon à couvrir les frais inhérents au site.
+            {t("AboutUs-5")}
             <br />
-            Par cette convention, où l’association devient propriétaire du site, la pérennité mémorielle de l’histoire
-            des sauveteurs du dunkerquois est assurée.
+            {t("AboutUs-6")}
           </Typography>
         </Container>
       </div>

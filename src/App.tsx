c69b1410998ffb18e03e9198b6aca207f0fb500e
Mathis Engels <mathisengels@yahoo.fr>
2021-12-03 03:40:51 +0100

    Fix drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "pages/Home";
+import LoremIpsum from "pages/LoremIpsum";
 
 const App = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/missions" element={<LoremIpsum title="missions" />} />
+    <Route path="/harbor-services" element={<LoremIpsum title="harbor-services" />} />
+    <Route path="/maritime-savings" element={<LoremIpsum title="maritime-savings" />} />
+    <Route path="/technics" element={<LoremIpsum title="technics" />} />
+    <Route path="/history" element={<LoremIpsum title="history" />} />
+    <Route path="/additional" element={<LoremIpsum title="additional" />} />
+    <Route path="/estaminet" element={<LoremIpsum title="estaminet" />} />
   </Routes>
 );
 
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { AppBar, Box, Container, Drawer, IconButton, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Container, Divider, Drawer, IconButton, Link, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import "./MainLayout.scss";
 import Search from "components/SearchBar";
@@ -20,7 +20,9 @@ const MainLayout: React.FC = ({ children }) => {
             <MenuIcon />
           </IconButton>
           <Box className="title">
-            <img src={logo} alt="logo" className="logo" />
+            <Link href="/">
+              <img src={logo} alt="logo" className="logo" />
+            </Link>
             <Box className="title-text">
               <Typography variant="h5" component="div">
                 Sauveteur du dunkerquois
@@ -36,12 +38,62 @@ const MainLayout: React.FC = ({ children }) => {
       </AppBar>
       <div className="toolbar-offset" />
       <div className="content">{children}</div>
-
       <Container className="footer">footer</Container>
-      <Drawer anchor="left" open={drawerState} onClose={() => toggleDrawer()}>
-        <div>a</div>
-        <div>b</div>
-        <div>c</div>
+      <Drawer className="Drawer" anchor="left" open={drawerState} onClose={() => toggleDrawer()}>
+        <Container>
+          <img className="drawer-logo" src={logo} alt="logo" />
+          <Divider />
+          <div className="drawer-item">
+            <Link href="/">
+              <Typography variant="h6">Accueil</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/rescuers">
+              <Typography variant="h6">Sauveteurs</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/sea-trips">
+              <Typography variant="h6">Sorties en mer</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/missions">
+              <Typography variant="h6">Sauvetages</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/harbor-services">
+              <Typography variant="h6">Services du port</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/maritime-savings">
+              <Typography variant="h6">Moyen maritimes</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/technics">
+              <Typography variant="h6">Techniques</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/history">
+              <Typography variant="h6">Historique</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/additional">
+              <Typography variant="h6">Compléments</Typography>
+            </Link>
+          </div>
+          <div className="drawer-item">
+            <Link href="/estaminet">
+              <Typography variant="h6">Estaminet</Typography>
+            </Link>
+          </div>
+        </Container>
       </Drawer>
     </>
   );
